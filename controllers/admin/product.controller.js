ee23dc35d0f1d304f8b273bf83e76ee23707d4a9
hotonbao <htbqn2003@gmail.com>
2024-02-22 12:40:36 +0700

    list products

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,7 +1,13 @@
 // GET  /admin/products
+const Product = require ('../../models/product.model')
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        deleted: false
+    })
+    console.log(products)
 
-module.exports.index = (req, res) => {
     res.render('admin/pages/products/index',{
-        pageTitle: "danh sach san pham"
+        pageTitle: "danh sach san pham",
+        products: products
     });
 }
