a628d5552a209119232f5a83c79300d0421a86cc
hotonbao <htbqn2003@gmail.com>
2024-03-05 21:43:09 +0700

    see detail account and change info of account

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -5,6 +5,7 @@ const productCategoryRoutes = require("./product-category.route");
 const roleRoutes = require("./role.route");
 const accountRoutes = require("./account.route");
 const authRoutes = require("./auth.route");
+const myAccountRoutes = require("./my-account.route");
 
 const systemConfig = require("../../config/system");
 module.exports = (app) => {
@@ -24,4 +25,6 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + "/roles", authMiddleware.requireAuth, roleRoutes);
   app.use(PATH_ADMIN + "/accounts", authMiddleware.requireAuth, accountRoutes);
   app.use(PATH_ADMIN + "/auth", authRoutes);
+  app.use(PATH_ADMIN + "/my-account", authMiddleware.requireAuth, myAccountRoutes);
+
 };
---
+++ routes/admin/my-account.route.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const multer = require("multer");
+const router = express.Router();
+
+const upload = multer();
+
+const controller = require("../../controllers/admin/my-account.controller");
+
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middleware");
+
+router.get("/", controller.index);
+
+router.get("/edit", controller.edit);
+
+router.patch(   
+    "/edit",
+    upload.single("avatar"),
+    uploadCloud.upload,
+    controller.editPatch
+);
+
+module.exports = router;
