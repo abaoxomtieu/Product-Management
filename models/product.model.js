383bcf55e4335e3b53f9254e2d9749121bec88cf
hotonbao <htbqn2003@gmail.com>
2024-03-06 08:39:43 +0700

    add highlight product field on admin site

--- models/product.model.js
+++ models/product.model.js
@@ -14,6 +14,7 @@ const productSchema = new mongoose.Schema(
     stock: Number,
     thumbnail: String,
     status: String,
+    featured: String,
     position: Number,
     slug: { type: String, slug: "title", unique: true },
     createdBy: {
