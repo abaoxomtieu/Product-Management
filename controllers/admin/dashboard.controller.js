bfa47f5c0be2811198ffbc6096130101b25d4db6
hotonbao <htbqn2003@gmail.com>
2024-03-17 10:31:43 +0700

    done dashboard page on admin site

--- controllers/admin/dashboard.controller.js
+++ controllers/admin/dashboard.controller.js
@@ -1,7 +1,84 @@
 // GET  /admin/dashboard
+const ProductCategory = require("../../models/product-category.model")
+const Product = require("../../models/product.model")
+const Account = require("../../models/account.model")
+const User = require("../../models/user.model")
+module.exports.dashboard = async (req, res) => {
+  const statistic = {
+    categoryProduct: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    product: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    account: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+    user: {
+      total: 0,
+      active: 0,
+      inactive: 0,
+    },
+  };
+  //Category Product
+  statistic.categoryProduct.total = await ProductCategory.countDocuments({
+    deleted: false,
+  })
+  statistic.categoryProduct.active = await ProductCategory.countDocuments({
+    deleted: false,
+    status: 'active',
+  })
+  statistic.categoryProduct.inactive = await ProductCategory.countDocuments({
+    deleted: false,
+    status: 'inactive',
+  })
+  //Product
+  statistic.product.total = await Product.countDocuments({
+    deleted: false,
+  })
+  statistic.product.active = await Product.countDocuments({
+    deleted: false,
+    status: 'active',
+  })
+  statistic.product.inactive = await Product.countDocuments({
+    deleted: false,
+    status: 'inactive',
+  })
+  //Admin account
+  statistic.account.total = await Account.countDocuments({
+    deleted: false,
+  })
+  statistic.account.active = await Account.countDocuments({
+    deleted: false,
+    status: 'active',
+  })
+  statistic.account.inactive = await Account.countDocuments({
+    deleted: false,
+    status: 'inactive',
+  })
+  //Client account 
+  statistic.user.total = await User.countDocuments({
+    deleted: false,
+  })
+  statistic.user.active = await User.countDocuments({
+    deleted: false,
+    status: 'active',
+  })
+  statistic.user.inactive = await User.countDocuments({
+    deleted: false,
+    status: 'inactive',
+  })
 
-module.exports.dashboard = (req, res) => {
-    res.render('admin/pages/dashboard/index',{
-        pageTitle: "trang tong quan"
-    });
-}
+
+
+  res.render("admin/pages/dashboard/index", {
+    pageTitle: "trang tổng quan",
+    statistic: statistic,
+  });
+};
