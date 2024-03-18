9240bca3c929bade4fd0e404d4904e2fa67e9bc3
hotonbao <htbqn2003@gmail.com>
2024-03-18 09:17:55 +0700

    add insert icon in chat use library "emoji-picker-element"

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,3 +1,5 @@
+import * as Popper from "https://cdn.jsdelivr.net/npm/@popperjs/core@^2/dist/esm/index.js";
+
 // CLIENT SEND MESSAGE
 
 const formSendData = document.querySelector(".inner-form");
@@ -36,10 +38,41 @@ socket.on("SERVER_RETURN_MESSAGE", (data) => {
 
 //Scroll Chat to bottom function
 function scrollChatToBottom() {
-    const bodyChat = document.querySelector(".chat .inner-body");
-    if(bodyChat){
-      bodyChat.scrollTop = bodyChat.scrollHeight;
-    }
+  const bodyChat = document.querySelector(".chat .inner-body");
+  if (bodyChat) {
+    bodyChat.scrollTop = bodyChat.scrollHeight;
   }
+}
 scrollChatToBottom();
 // End Scroll Chat to bottom
+
+//Emoji picker icon chat
+
+//Show popup
+const buttonIcon = document.querySelector(".button-icon");
+console.log(buttonIcon);
+if (buttonIcon) {
+  const tooltip = document.querySelector(".tooltip");
+  Popper.createPopper(buttonIcon, tooltip);
+
+  buttonIcon.onclick = () => {
+    tooltip.classList.toggle("shown");
+  };
+}
+//End show popup
+
+//Inser icon to input
+const emojiPicker = document.querySelector("emoji-picker");
+if (emojiPicker) {
+  emojiPicker.addEventListener("emoji-click", (event) => {
+    const icon = event.detail.unicode;
+    const input = document.querySelector(
+      ".chat .inner-form input[name='content']"
+    );
+    input.value = input.value + icon;
+    
+  });
+}
+//End insert icon to input
+
+//End Emoji picker icon chat
