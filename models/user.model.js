a1e6640771f300007b70e4a056793ee2fe0dfceb
hotonbao <htbqn2003@gmail.com>
2024-03-22 09:35:42 +0700

    add more field to user collection

--- models/user.model.js
+++ models/user.model.js
@@ -16,6 +16,15 @@ const userSchema = new mongoose.Schema(
       type: String,
       default: "active",
     },
+    requestFriends: Array,
+    acceptFriends: Array,
+    friendList: [
+      {
+        user_id: String,
+        room_chat_id: String,
+      },
+    ],
+
     deleted: {
       type: Boolean,
       default: false,
